7fbe8c53fada71345e677a74be49fecd624db769
BiswasSwagatam <swagatam.04.biswas@gmail.com>
2024-09-30 20:23:42 +0530

    auth-controllers added

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.route.js";
 import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config()
 
@@ -10,6 +11,8 @@ const app = express();
 const PORT = process.env.PORT || 5000
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
 
 app.use('/api/auth', authRoutes);
 
