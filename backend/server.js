4805a1d4ebd25826be6d3ea6d9759bd5a2a56d93
BiswasSwagatam <swagatam.04.biswas@gmail.com>
2024-09-30 17:50:20 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,9 @@ import connectMongoDB from "./db/connectMongoDB.js";
 dotenv.config()
 
 const app = express();
-
 const PORT = process.env.PORT || 5000
+
+app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 
