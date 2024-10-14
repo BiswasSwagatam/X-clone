ab63a9067860d91ca54726a111c6865d644e313c
BiswasSwagatam <swagatam.04.biswas@gmail.com>
2024-10-14 12:22:02 +0530

    project completed

--- backend/server.js
+++ backend/server.js
@@ -21,14 +21,14 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 5000
 
-app.use(express.json({limit:"5mb"}));
+app.use(express.json({limit:"10mb"}));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
-app.use('/api/notification', notificationRoutes);
+app.use('/api/notifications', notificationRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server started on ${PORT}`);
